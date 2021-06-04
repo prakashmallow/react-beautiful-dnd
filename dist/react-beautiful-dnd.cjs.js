130289dc89f8646c4e21a6ac67ddfb7fd2864715
Rajesh kumar <rajeshkumar@mallow-tech.com>
2021-06-04 12:37:46 +0530

    Added function for getting cursor offset

--- dist/react-beautiful-dnd.cjs.js
+++ dist/react-beautiful-dnd.cjs.js
@@ -466,6 +466,16 @@ function toArray(list) {
   return Array.prototype.slice.call(list);
 }
 
+var getOffsetValues = memoizeOne(function (draggableId) {
+  var draggableElement = document.querySelector("[data-rbd-draggable-id=\"" + draggableId + "\"]");
+  var offsetX = parseInt(draggableElement.getAttribute('data-offset-x'), 10);
+  var offsetY = parseInt(draggableElement.getAttribute('data-offset-y'), 10);
+  return {
+    offsetX: offsetX,
+    offsetY: offsetY
+  };
+});
+
 var toDroppableMap = memoizeOne(function (droppables) {
   return droppables.reduce(function (previous, current) {
     previous[current.descriptor.id] = current;
@@ -518,15 +528,6 @@ var getDraggableParentId = function getDraggableParentId(droppables, isHome, cen
   }, isManualTrigger);
   return Object.keys(foldersLIst)[0];
 };
-var getOffsetValues = function getOffsetValues(draggableId) {
-  var draggableElement = document.querySelector("[data-rbd-draggable-id=\"" + draggableId + "\"]");
-  var offsetX = parseInt(draggableElement.getAttribute('data-offset-x'));
-  var offsetY = parseInt(draggableElement.getAttribute('data-offset-y'));
-  return {
-    offsetX: offsetX,
-    offsetY: offsetY
-  };
-};
 var getDroppableList = function getDroppableList(draggable, pageBorderBox, droppables) {
   var _getOffsetValues = getOffsetValues(draggable.descriptor.id),
       offsetX = _getOffsetValues.offsetX,
@@ -536,7 +537,7 @@ var getDroppableList = function getDroppableList(draggable, pageBorderBox, dropp
   var y = offsetY ? offsetY + pageBorderBox.top : pageBorderBox.center.y;
   var isManualTrigger = !(isNaN(offsetX) && isNaN(offsetY));
   var isFullScreen = document.elementsFromPoint(x, y).some(function (ele) {
-    return ele.className.includes('ant-modal-content') && !!ele.closest('.fullscreen-folder-modal');
+    return ele.className.includes('ant-modal-content') && Boolean(ele.closest('.fullscreen-folder-modal'));
   });
 
   if (isFullScreen) {
@@ -584,7 +585,7 @@ var getDroppableList = function getDroppableList(draggable, pageBorderBox, dropp
   if (topElement) {
     var _values3;
 
-    var draggableParentId = getDraggableParentId(droppables, !!isHome, {
+    var draggableParentId = getDraggableParentId(droppables, Boolean(isHome), {
       x: x,
       y: y
     }, isManualTrigger);
